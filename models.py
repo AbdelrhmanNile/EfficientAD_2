3c1d4fffa94068df708e19d18f6f5a2e9f85f718
Abdelrhman Nile <abdelrhmannile@gmail.com>
2024-12-09 15:19:58 +0200

    test

--- models.py
+++ models.py
@@ -365,4 +365,8 @@ if __name__ == '__main__':
     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = AutoEncoder()
     model = model.to('cuda')
-    summary(model, (3, 256, 256))
+    summary(model, (3, 1024, 1024))
+
+    model = Teacher('S')
+    model = model.to('cuda')
+    summary(model, (3, 1024, 1024))
